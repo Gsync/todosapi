475a24d84675506c40449d3a764bf3f5b69f8f03
Khuram Niaz <knshaikh@gmail.com>
2017-10-24 13:12:05 -0600

    display completed todo using addClass

--- public/app.js
+++ public/app.js
@@ -7,6 +7,9 @@ function addTodos(todos) {
     //add todos to the page here
     todos.forEach(function(todo) {
         var Todo = $("<li class='task'>" + todo.name + "</li>");
+        if (todo.completed) {
+            Todo.addClass("done");
+        }
         $(".list").append(Todo);        
     }, this);
 }
