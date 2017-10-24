618f5d30771f236717ad9b832294534f989ebf46
Khuram Niaz <knshaikh@gmail.com>
2017-10-23 18:09:30 -0600

    Refactor code 2

--- routes/todos.js
+++ routes/todos.js
@@ -4,14 +4,22 @@ var router = express.Router();
 var db = require("../models") //directs to db models folder that incldes the schema
 var helpers = require("../helpers/todos");
 
-router.get("/", helpers.getTodos);
+// router.get("/", helpers.getTodos);
+// router.post("/", helpers.createTodo);
 
-router.post("/", helpers.createTodo);
+//Routes Refactored
+router.route("/")
+    .get(helpers.getTodos)
+    .post(helpers.createTodo);
 
-router.get("/:todoId", helpers.getTodo);
+// router.get("/:todoId", helpers.getTodo);
+// router.put("/:todoId", helpers.updateTodo);
+// router.delete("/:todoId", helpers.deleteTodo);
 
-router.put("/:todoId", helpers.updateTodo);
-
-router.delete("/:todoId", helpers.deleteTodo);
+//Routes Refactored
+router.route("/:todoId")
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo);
 
 module.exports = router;
